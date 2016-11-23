2e0917788c34659ab634b43697dce5abef4ef7ce
cirquit <alex.isenko@googlemail.com>
2016-11-23 22:23:47 +0100

    some scetches for 3-1-1, 3-1-2

--- assignments/a-03/solution/find_mean_rep.h
+++ assignments/a-03/solution/find_mean_rep.h
@@ -9,14 +9,40 @@
 
 namespace cpppc {
 
+
+// naive implementation O(2n)
 template <class Iter>
 Iter find_mean_rep(Iter first, Iter last) {
   typedef typename std::iterator_traits<Iter>::value_type value_t;
 
-  // YOUR IMPLEMENTATION HERE
+  if (first == last) return last;
 
+  double  mean = 0;
+  int        n = 0;
+  value_t  sum = 0;
+
+  for(auto iter = first; iter != last; iter++){
+    sum += &iter;
+    n   += 1;
+  }
+
+  mean = sum / n;
+  value_t diff = abs(mean - &first);
+  Iter cur = first;
+
+  for(auto iter = (first+1); iter != last; iter++){
+    if (abs((mean - &iter)) < diff){
+      diff = abs(mean - &iter);
+      cur  = iter;
+    }
+  }
+
+  return cur;
 }
 
 } // namespace cpppc
+
+
+
 
 #endif // CPPPC__S03__FIND_MEAN_REP_H__INCLUDED
--- assignments/a-03/solution/histogram.h
+++ assignments/a-03/solution/histogram.h
@@ -6,14 +6,45 @@
 #include <set>
 #include <algorithm>
 #include <iterator>
+#include <functional>
 
+// if an element is found, the supplied function will take this element as the first argument
+// and the value as the second
+template <typename K, typename V>
+void insertWith(K key, V value, std::unordered_map<K,V> umap, std::function<V (V,V)> f) {
+
+    auto miter = umap.find(key);
+    if(miter == umap.end()) {
+        umap[key] = value;
+    } else {
+        umap[key] = f(&miter, value);
+    }
+}
 
 template <class Iter>
 inline Iter histogram(Iter first, Iter last) {
     typedef typename std::iterator_traits<Iter>::value_type      value_t;
     typedef typename std::iterator_traits<Iter>::difference_type index_t;
 
-    // YOUR IMPLEMENTATION HERE
+    std::unordered_map<int,value_t> umap;
+    std::vector<value_t>            res;
+
+    for(auto iter = first; iter != last; ++iter){
+        insertWith(&iter, 1, umap,
+            [](int a, int b) -> int { return a + b; }
+        );
+    }
+
+   for(auto iter = first; iter != last; ++iter){
+      auto miter = umap.find(&iter);
+      if(miter != umap.end()){
+          res.push_back(&miter);
+          umap.erase(miter);
+      }
+   }
+
+   // with this solution we should return the begin & end
+   return res.end();
 }
 
 #endif // CPPPC__S03__HISTOGRAM_H__INCLUDED
